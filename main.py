ad6f447fcae2e574e05cd5f8f7afe809e22db0d4
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-17 16:53:25 -0300

    Adding main script

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from blockchain import Blockchain
 
-
 blockchain = Blockchain()
 
-blockchain.add_new_block("YOO")
+blockchain.add_new_block("First block")
+blockchain.add_new_block("Second block")
 blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-blockchain.add_new_block("Third block")
-
+blockchain.add_new_block("Fourth block")
+blockchain.add_new_block("Fifth block")
+blockchain.add_new_block("Sixth block")
+blockchain.add_new_block("Seventh block")
+blockchain.add_new_block("Eight block")
+blockchain.add_new_block("Ninth block")
+blockchain.add_new_block("Tenth block")
 
 for block in blockchain.get_blocks():
-    print(block)
+    print()
+    print('\t' + block.to_string())
+
+print()
